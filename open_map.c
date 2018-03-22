211566d9f63be6f3d4a28cae9c6b4904452c7c69
KeefeVandenberg <keefev1932@gmail.com>
2018-03-21 18:02:54 -0700

    working map with most error handling, we will see what comes up

--- open_map.c
+++ open_map.c
@@ -6,13 +6,13 @@
 /*   By: kvandenb <kvandenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 17:21:48 by kvandenb          #+#    #+#             */
-/*   Updated: 2018/03/20 23:14:48 by kvandenb         ###   ########.fr       */
+/*   Updated: 2018/03/21 18:02:24 by kvandenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-int	*parse(char *str, int x)
+int		*parse(char *str, int x)
 {
 	int *arr;
 	int i;
@@ -36,7 +36,7 @@ int	*parse(char *str, int x)
 	return (arr);
 }
 
-int	*surround(char *str, int size)
+int		*surround(char *str, int size)
 {
 	int *i_str;
 	int k;
@@ -51,30 +51,70 @@ int	*surround(char *str, int size)
 	return (i_str);
 }
 
-int	ft_initread(char *str, t_env *all)
+void	ft_finish_malloc(int index, t_env *e)
 {
-	t_env	*current;
+	t_env *all;
+
+	all = e;
+	while (index != HEIGHT_MAP - 1)
+	{
+		all->map[index] = malloc(sizeof(int *) * 5);
+		index++;
+	}
+}
+
+int		ft_check_validread(char *buf, int fd, t_env *all, int index)
+{
+	int i;
+
+	if (fd < 0)
+	{
+		ft_finish_malloc(index, all);
+		ft_putendl("Error : not a valid file");
+		ft_exit(all);
+	}
+	if (index < HEIGHT_MAP - 1 || all->bytes_read < 36)
+	{
+		ft_finish_malloc(index, all);
+		ft_putendl("Error : map input is too small or characters in map");
+		ft_exit(all);
+	}
+	read(fd, &buf, 1);
+	i = ft_strlen(buf);
+	if (i == 0)
+		return (1);
+	if (i > 0)
+	{
+		ft_putendl("Error : map input is either invalid or too big");
+		ft_exit(all);
+	}
+	return (0);
+}
+
+int		ft_initread(char *str, t_env *current)
+{
 	int		fd;
 	int		index;
 	char	buf[42];
 
 	index = -1;
-	current = all;
 	ft_bzero(buf, 42);
-	if (0 > (fd = open(str, O_RDONLY)))
-	{
-		ft_putendl("Error : not valid file");
-		exit(1);
-	}
 	current->map = (int **)malloc(sizeof(int **) * HEIGHT_MAP);
-	while (35 < (read(fd, &buf, 36)))
+	if (0 > (fd = open(str, O_RDONLY)))
+		ft_check_validread(buf, fd, current, index + 1);
+	while ((current->bytes_read =
+		(read(fd, &buf, 36))) && index++ != HEIGHT_MAP - 1)
 	{
-		if (++index == 0 || index == HEIGHT_MAP)
-			current->map[index] = surround(buf, WIDTH_MAP);
-		else
-			current->map[index] = parse(buf, WIDTH_MAP);
+		if ((ft_checkcharacters(buf)) == 0)
+			ft_check_validread(buf, fd, current, index);
+		current->map[index] = ((index == 0 || index == HEIGHT_MAP)
+			? surround(buf, WIDTH_MAP) : parse(buf, WIDTH_MAP));
+		if (ft_strlen(ft_strstr(buf, "\n")) > 1 || current->bytes_read < 36)
+			ft_check_validread(buf, fd, current, index);
 		ft_bzero(buf, 42);
 	}
+	current->bytes_read = 36;
+	ft_check_validread(buf, fd, current, index);
 	close(fd);
 	return (0);
 }
